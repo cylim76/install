050f0d8912114ec8e1b47bac9badd2c795508982
cylim76 <cylim76@gmail.com>
2022-12-10 09:26:12 +0800

    Create tvlist.sh

---
+++ iptv/tvlist.sh
@@ -0,0 +1,20 @@
+# kozalak-robot, 央视国内卫视 
+https://raw.githubusercontent.com/kozalak-robot/iptv/master/daily.m3u8
+https://raw.githubusercontent.com/kozalak-robot/iptv/master/daily_multi.m3u8
+
+#lanjing_live, IP v6专用
+https://raw.githubusercontent.com/Cyril0563/lanjing_live/2ba8f6975b0b520a760380fda33c61ae6bc3ffc6/IPTV_FREE/IPV6%E4%B8%93%E7%94%A8.txt
+
+
+#YueChan, 100HD国内高清台
+https://raw.githubusercontent.com/YueChan/IPTV/main/IPTV.m3u
+https://github.com/YueChan/Live/raw/main/IPTV.m3u
+
+#gjwj666/jichang ,adult VOD
+https://github.com/gjwj666/jichang/raw/main/chuanmei.m3u
+https://github.com/gjwj666/jichang/raw/main/chuanmei2.m3u
+
+#YanG-1989/m3u, adult vod
+https://raw.githubusercontent.com/YanG-1989/m3u/main/Adult.m3u
+
+
