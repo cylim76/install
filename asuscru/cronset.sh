1cf72e152bb6108d9d9d902777379e0c35609eb8
cylim76 <cylim76@gmail.com>
2024-02-25 10:52:02 +0800

    Update cronset.sh

--- asuscru/cronset.sh
+++ asuscru/cronset.sh
@@ -1,2 +1,11 @@
-cru a ppgengxin "3 6-21 * * * /koolshare/scripts/pushplus_check_task.sh"
+dbus set ss_basic_enable=0
+sh /koolshare/ss/ssconfig.sh stop
+
+/koolshare/scripts/ss_online_update.sh fancyss 3
+
+dbus set ss_basic_enable=1
+sh /koolshare/ss/ssconfig.sh restart
+
 cru a ssgengxin "*/28 3,4,5,6,7,12,15,17,21 * * * /koolshare/scripts/ss_online_update.sh fancyss 3"
+cru a pplusgengxin "3 6-21 * * * /koolshare/scripts/pushplus_check_task.sh"
+cru a asusset "*/10  * * * /root/cronset.sh"
